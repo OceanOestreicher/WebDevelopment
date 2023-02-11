8ec57c22d098796e677f722cc112140747d596e9
Ocean Oestreicher <ooestreicher.ewu.edu>
2023-02-11 12:08:26 -0800

    Database set up and linked with login

--- node/server.js
+++ node/server.js
@@ -3,17 +3,44 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 var fs = require("fs");
+const { connect } = require('http2');
+
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'project_database'
+});
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Database successfully connected');
+});
 
  app.get('/listUsers', function (req, res) {
-    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.end( data );
-    });
+    connection.query('SELECT * FROM accounts', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+      });
+      res.end(rows);
  })
  app.post('/validateLogin', function (req, res) {
-    var params = req.body;
     var email = req.body["accountEmail"];
     var password = req.body["accountPassword"];
+    let sql = 'SELECT count(accountId) as valid FROM accounts where ? = email and ? =password'
+    connection.query(sql,[email,password], (err,rows) => {
+        if(err) throw err;
+        console.log('Data received from Db:');
+        console.log(rows);
+        rows.forEach(row => {
+            console.log(row.valid);
+            if(row.valid == 0)res.end("Invalid Credentials");
+            else res.end("Credentials Validated");
+        });
+      });
+      /*
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
         var users = JSON.parse(data);
         for(const user in users){
@@ -23,11 +50,11 @@ var fs = require("fs");
             }
             
         }
-    });
+    });*/
  })
 
 var server = app.listen(3000, function () {
     var host = server.address().address
     var port = server.address().port
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Server listening at http://%s:%s", host, port)
  })
