a9215c1ac1822565f5adb25966634003f7075052
Ocean Oestreicher <ooestreicher.ewu.edu>
2023-02-11 11:09:50 -0800

    Login basic validation implemented

--- node/server.js
+++ node/server.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 var fs = require("fs");
 
  app.get('/listUsers', function (req, res) {
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
+    });
+ })
+ app.post('/validateLogin', function (req, res) {
+    var params = req.body;
+    var email = req.body["accountEmail"];
+    var password = req.body["accountPassword"];
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        var users = JSON.parse(data);
+        for(const user in users){
+            if(users[user]["email"]==email){
+                if(users[user]["password"]==password)res.end("Credentials Validated");
+                else res.end("Invalid Credentials");
+            }
+            
+        }
     });
  })
 
